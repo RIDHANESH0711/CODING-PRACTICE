5af1c7b34717506cd52d594a076d2ec4dfccb828
RIDHANESH T <your-github-ridhan0711@gmail.com>
2026-07-20 17:22:50 +0530

    Updated Java code

--- rcb.java
+++ rcb.java
@@ -1,7 +1,26 @@
 import java.util.Scanner;
 class RCB {
     public static void main(String[] args) {
-        System.out.println("roopesh");
-    }
+      //  System.out.println("roopesh");
+       /* Scanner a=new Scanner(System.in);
+        int m=a.nextInt();
+        int n=a.nextInt();
+      for(int i=m;i<=n;i++){
+        if(i%3==0 && i%5==0){
+            System.out.println("RCB");
+        }
+        else if(i%3==0){
+            System.out.println("RC");
+        }
+        else if(i%5==0){
+            System.out.println("B");
+        }
+        else{
+            System.out.println(i);
+        }
+    }*/
+   // array:
+   
     
-}
+}}
+
