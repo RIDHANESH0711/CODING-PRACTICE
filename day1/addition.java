13eefa98da907d17ccbf8654589da154d38c6591
RIDHANESH T <your-github-ridhan0711@gmail.com>
2026-07-27 22:34:33 +0530

    day 1 completed

---
+++ day1/addition.java
@@ -0,0 +1,12 @@
+public class addition {
+    int a = 10;
+    int b = 20;
+    
+    void sum(){
+     System.out.print(a + b);
+    }
+    public static void main(String[]args){
+        addition f= new addition();
+        f.sum();
+    }
+}
