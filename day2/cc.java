8e784e9e50ce75d4010c921159222e666b4c9234
RIDHANESH0711 <ridhan0711@gmail.com>
2026-07-30 21:40:40 +0530

    dayy ..

---
+++ day2/cc.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Student {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Age: ");
+        int age = sc.nextInt();
+
+        System.out.print("Enter CGPA: ");
+        double cgpa = sc.nextDouble();
+
+        System.out.println();
+
+        System.out.println("Student Details");
+        System.out.println("Name : " + name);
+        System.out.println("Age : " + age);
+        System.out.println("CGPA : " + cgpa);
+    }
+}
