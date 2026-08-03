33680e43970989b358169441994c250c5de4b3be
RIDHANESH0711 <ridhan0711@gmail.com>
2026-08-03 16:15:36 +0530

    hello1

--- lap.java
+++ lap.java
@@ -14,7 +14,7 @@ public class lap {/*
 
     
 
-   }*/
+   }
     
 }
 import java.util.Scanner;
@@ -33,5 +33,29 @@ public class ReverseString {
         System.out.println("Reversed String: " + reverse);
 
         sc.close();
+    }*/
+    import java.util.Scanner;
+
+public class ArithmeticExample {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("Addition = " + (a + b));
+        System.out.println("Subtraction = " + (a - b));
+        System.out.println("Multiplication = " + (a * b));
+        System.out.println("Division = " + (a / b));
+        System.out.println("Remainder = " + (a % b));
     }
 }
+
+
+
+}   
+
